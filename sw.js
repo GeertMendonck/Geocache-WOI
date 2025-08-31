c3d129809418296870cb3dc42775fa9ed34c5c13
GeertMendonck <geert.mendonck@leerkracht.sintlodewijkantwerpen.be>
2025-08-31 14:37:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
-// sw.js – voeg je data toe
-const CACHE = 'woi-pwa-v7';
+// sw.js
+const CACHE = 'woi-pwa-v10';
 const PRECACHE = [
   './',
   './index.html',
   './manifest.webmanifest',
+  './js/app.js',
   './data/meta.json',
   './data/stops.json',
   './data/personages.json',
+  './data/route.gpx',   // of route.kml (of beide)
   './data/route.kml'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
-    await self.clients.claim();
-  })());
-});
+// install/activate zoals je al had…
 
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
+
   // HTML: network-first
   if (e.request.mode === 'navigate' || e.request.destination === 'document') {
     e.respondWith(fetch(e.request).then(res => {
@@ -31,14 +24,16 @@ self.addEventListener('fetch', (e) => {
     }).catch(() => caches.match('./index.html')));
     return;
   }
-  // JSON & KML: network-first (zodat updates zonder code doorstromen)
-  if (url.pathname.endsWith('.json') || url.pathname.endsWith('.kml')) {
+
+  // JSON/KML/GPX: network-first (zodat data-updates zónder code ook doorstromen)
+  if (/\.(json|kml|gpx)$/i.test(url.pathname)) {
     e.respondWith(fetch(e.request).then(res => {
       caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res;
     }).catch(() => caches.match(e.request)));
     return;
   }
-  // overige assets: cache-first
+
+  // Overig (incl. js/css/img): cache-first
   e.respondWith(caches.match(e.request).then(cached =>
     cached || fetch(e.request).then(res => {
       if (url.origin === location.origin && e.request.method === 'GET') {
