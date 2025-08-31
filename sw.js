7c44b7514cd1f6f397e4b6fe1aff67ccabb5de16
GeertMendonck <geert.mendonck@leerkracht.sintlodewijkantwerpen.be>
2025-08-31 09:58:03 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js – ultrakleine offline cache
+const CACHE = 'woi-pwa-v1';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(res => {
+        try {
+          if (e.request.method === 'GET' &&
+              new URL(e.request.url).origin === location.origin) {
+            const copy = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, copy));
+          }
+        } catch {}
+        return res;
+      }).catch(() => caches.match('./'))
+    )
+  );
+});
